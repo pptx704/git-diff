b7c91d52fc40d48b156993902080d21af394b676
FK12344321 <fk0606@yandex.ru>
2023-09-30 21:22:08 +0300

    cors

--- controller/repositoryMetaController.js
+++ controller/repositoryMetaController.js
@@ -1,7 +1,10 @@
 const repositoryOps = module.require('../repository/RepositoryOps')
+const cors = module.require('cors')
 
 const express = require('express');
 const app = express();
+
+app.use(cors());
 const port = 3000;
 
 const repoPath = 'git-repos/casey'
@@ -17,5 +20,12 @@ app.get('/branches', async (req, res) => {
 
 app.get('/commits', async (req, res) => {
     let commits = await repositoryOps.getCommits(repoPath, req.query.branch)
+    res.json(commits);
+});
+
+app.get('/diff', async (req, res) => {
+    let originalHash = req.query.original
+    let updatedHash = req.query.updated
+    let commits = await repositoryOps.getDiff(repoPath, originalHash, updatedHash)
     res.json(commits);
 });
